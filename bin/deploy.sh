9d56bc27702052d68126fce6084d65b8dd72d64f
Marcos Lin <linm.macbookair@ebacimail.com>
2015-03-10 17:45:13 +0100

    First working version of merchantpay

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,5 +11,5 @@ rsync --exclude ".*" -av "$source_dir/root/" "$dest_dir"
 # Copy Projects
 for dir in merchantpay;
 do
-	rsync --exclude ".*" -av "$source_dir/$dir" "$dest_dir/$dir"
+	rsync --exclude ".*" -av "$source_dir/$dir/" "$dest_dir/$dir/"
 done
