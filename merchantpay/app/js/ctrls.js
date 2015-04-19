60dccfd9319a6dad0774eb22e0f8adadd2914c56
Marcos Lin <linm.macbookair@ebacimail.com>
2015-04-19 16:32:18 +0200

    Updated formatting

--- merchantpay/app/js/ctrls.js
+++ merchantpay/app/js/ctrls.js
@@ -1,20 +1,12 @@
-define(['angularAMD', 'services', 'notificationAAMD'], function (angularAMD) {
-    angularAMD.controller('main_ctrl', ['$scope', 'setting', 'Notification', '$log', function ($scope, setting, Notification, $log) {
-        setting.initialize($scope);
-        $scope.read = setting.read;
-        $scope.save = setting.save;
-        $log.log('main_ctrl.data', $scope.data);
-    	
-        $scope.$watchGroup(['data.transactionId','data.amount'], function () {
-			setting.calcHash();
-		});
+define(['angularAMD', 'setting', 'notificationAAMD'], function (angularAMD) {
+    angularAMD.controller('main_ctrl', ['$scope', 'Setting', 'Notification', '$log', function ($scope, Setting, Notification, $log) {
+        // Handle sessionId change event from watchChange directive
+        $scope.$on('watchChangeEvent', function (event, value) {
+            $log.log('watchChangeEvent value: ', value);
+            Setting.setSessionId(value);
+        });
         
-        $scope.loadScript = function () {
-            if ($scope.data.appId) {
-                $scope.data.jsUrl = setting.getJsUrl();
-            }
-        };
-        
+        // Handle load sessionId event from loadScript directive
         $scope.$on('loadScriptEvent', function (event, status) {
             $log.log('loadScriptEvent value: ', status);
         	if (status==='loading') {
@@ -27,15 +19,21 @@ define(['angularAMD', 'services', 'notificationAAMD'], function (angularAMD) {
                 Notification.clearMessage();
             }
         });
-        
-        $scope.$on('watchChangeEvent', function (event, value) {
-            $log.log('watchChangeEvent value: ', value);
-            setting.setSessionId(value);
-        });
+
+        // Initialize the setting that will load the data and bind the save/read method to $scope
+        Setting.initialize($scope);
         
         // Load the sessionId
+        $scope.loadScript = function () {
+            if ($scope.data.appId) {
+                $scope.data.jsUrl = Setting.getJsUrl();
+            }
+        };
         $scope.loadScript();
         
-        
+        // Watch for changes in variable needed by validation hash
+        $scope.$watchGroup(['data.transactionId','data.amount'], function () {
+			Setting.calcValidationHash();
+		});
     }]);
 });
